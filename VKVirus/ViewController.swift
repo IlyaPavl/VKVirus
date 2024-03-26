5fda33784c415de77bd96b26e3fbfb216699273a
Ilya Pavlov <ilyapav@icloud.com>
2024-03-26 21:26:57 +0300

    Initial Commit

---
+++ VKVirus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKVirus
+//
+//  Created by ily.pavlov on 26.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
